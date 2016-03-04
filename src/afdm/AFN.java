eccfe4ad234356ab68162d65494afc7891f42696
justo <justo@justo-pc>
2016-03-04 00:59:01 -0600

    cambio en AFN

--- src/afdm/AFN.java
+++ src/afdm/AFN.java
@@ -10,17 +10,35 @@ package afdm;
  * @author justo
  */
 public class AFN {
+    Estructura estructura = new Estructura();
 
     public AFN(String[][] datos, int alto, int ancho) {
-        for(int i=0;i<alto;i++)
-        {
-            if(i==0)
-            {
-                //Estructura.comprobarSiExiste(datos[0][ancho-1]==true)
-            }
-        }
+        String[][] matrizAFN = new String[alto][ancho];
+        NodoAFN[][] matrizAFD = new NodoAFN[alto][1];
+        crearEstadoInicial(matrizAFN,matrizAFD,ancho);
+        
+      
     }
     
+    public void crearEstadoInicial(String[][] AFN, NodoAFN[][] AFDM, int ancho)
+    {
+        AFDM[0][0].setNombre(AFN[0][0]+","+AFN[0][AFN.length-2]);
+        if((AFN[0][AFN.length-1]=="si")|| (AFN[0][AFN.length-1]=="si"))
+        {
+            AFDM[0][AFN.length-1].setNombre("si");
+        }
+        else
+            AFDM[0][AFN.length-1].setNombre("no");
+    }
     
+    public void llenarMatriz(NodoAFN[][] AFDM,int ancho)
+    {
+        for(int i=0;i<AFDM.length-2;i++)
+        {
+            
+        }
+    }
+   
+    }
     
-}
+
