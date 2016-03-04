2dc6ff26e1e346717c23c7e6571cb5bc2829464c
justo <justo@justo-pc>
2016-03-04 01:02:08 -0600

    cambio en afn 1:01

--- src/afdm/AFDm.java
+++ src/afdm/AFDm.java
@@ -18,14 +18,14 @@ public class AFDm {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String a = "5,7-6,5 6,5-7,6 7,6-5,7";
-        String c = "5,7-6,5 6,5-7,6 7,6-57";
-        int q = (a.indexOf("6,5-"));
-        String b = a.substring(q+4, q+7);
-        System.out.println(a);
-        //System.out.println(q);
-        //System.out.println(b);
-        System.out.println(a.contains("5,7-"));
-        System.out.println(a.equals(c));
+        int x=3;
+        int[][] a = new int[2][x];
+        
+        a[0][0]= 5;
+        a[0][1]= 5;
+        x=3;
+        a[0][2]= 5;
+        
+        
     }
 }
