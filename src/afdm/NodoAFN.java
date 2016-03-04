db0e25628f8bef8c2fe044ea661726a448973007
justo <justo@justo-pc>
2016-03-03 19:21:28 -0600

    creacion clases Apuntador y NodoAFN

---
+++ src/afdm/NodoAFN.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afdm;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author justo
+ */
+public class NodoAFN {
+    private String Nombre;
+    private ArrayList<Apuntador> apuntador = new ArrayList<Apuntador>();
+
+    public NodoAFN(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public ArrayList<Apuntador> getApuntador() {
+        return apuntador;
+    }
+
+    public void setApuntador(ArrayList<Apuntador> apuntador) {
+        this.apuntador = apuntador;
+    }
+    
+    public void agregarElemento(NodoAFN nodo, String transicion)
+    {
+        Apuntador nuevoElemento = new Apuntador(transicion,nodo);
+        apuntador.add(nuevoElemento);
+    }
+    
+    
+    
+}
