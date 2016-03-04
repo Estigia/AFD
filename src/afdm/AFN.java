07d93d09e24a6aef65f815d0f7655ef1356a563a
justo <justo@justo-pc>
2016-03-03 20:10:59 -0600

    creacion clases AFN

---
+++ src/afdm/AFN.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afdm;
+
+/**
+ *
+ * @author justo
+ */
+public class AFN {
+
+    public AFN(String[][] datos, int alto, int ancho) {
+        for(int i=0;i<alto;i++)
+        {
+            if(i==0)
+            {
+                //Estructura.comprobarSiExiste(datos[0][ancho-1]==true)
+            }
+        }
+    }
+    
+    
+    
+}
